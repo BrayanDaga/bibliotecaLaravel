077820465fdba0e4ca7e3953a3f903fcf3fcae9b
Brayan Vilchez Daga <brayandaga5@gmail.com>
2020-04-03 20:46:11 -0500

    Implementacion diseño Adminlte ------------------------------------ *Se agrego el controlador de inicio *Se agrego provider theme->lte *Cambio en welcome *Se agrego templates aside ,footer, header y layout *Se actualizaron las rutas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        view()->share('theme', 'lte');
         Schema::defaultStringLength(191);
     }
 }
